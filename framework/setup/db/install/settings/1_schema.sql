69b1f314714c5a1560140afd2c2489a1ef09094f
Alex Villacís Lasso <a_villacis@palosanto.com>
2017-07-26 12:48:09 -0500

    Cambio de tema a tenant para DINOMI ISO.

--- framework/setup/db/install/settings/1_schema.sql
+++ framework/setup/db/install/settings/1_schema.sql
@@ -8,6 +8,6 @@ INSERT INTO "settings" VALUES('language','en');
 INSERT INTO "settings" VALUES('default_rate','0.50');
 INSERT INTO "settings" VALUES('default_rate_offset','1');
 INSERT INTO "settings" VALUES('elastix_version_release','2.0.0');
-INSERT INTO "settings" VALUES('theme','elastixneo');
+INSERT INTO "settings" VALUES('theme','tenant');
 INSERT INTO "settings" VALUES('currency','$');
 COMMIT;
