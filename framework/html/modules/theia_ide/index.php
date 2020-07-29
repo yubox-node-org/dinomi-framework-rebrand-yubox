5d1238690703584777fa95826907d9b2c87ccc76
Carlos Villacís <carlos2000villacis@gmail.com>
2020-07-29 04:06:57 -0500

    Embebido
    Sehan embebido las páginas grafana, nodered, theia ide

---
+++ framework/html/modules/theia_ide/index.php
@@ -0,0 +1,42 @@
+<?php
+    /* vim: set expandtab tabstop=4 softtabstop=4 shiftwidth=4:
+  Codificación: UTF-8
+  +----------------------------------------------------------------------+
+  | Elastix version 0.5                                                  |
+  | http://www.elastix.com                                               |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2006 Palosanto Solutions S. A.                         |
+  +----------------------------------------------------------------------+
+  | Cdla. Nueva Kennedy Calle E 222 y 9na. Este                          |
+  | Telfs. 2283-268, 2294-440, 2284-356                                  |
+  | Guayaquil - Ecuador                                                  |
+  | http://www.palosanto.com                                             |
+  +----------------------------------------------------------------------+
+  | The contents of this file are subject to the General Public License  |
+  | (GPL) Version 2 (the "License"); you may not use this file except in |
+  | compliance with the License. You may obtain a copy of the License at |
+  | http://www.opensource.org/licenses/gpl-license.php                   |
+  |                                                                      |
+  | Software distributed under the License is distributed on an "AS IS"  |
+  | basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See  |
+  | the License for the specific language governing rights and           |
+  | limitations under the License.                                       |
+  +----------------------------------------------------------------------+
+  | The Original Code is: Elastix Open Source.                           |
+  | The Initial Developer of the Original Code is PaloSanto Solutions    |
+  +----------------------------------------------------------------------+
+  $Id: index.php,v 1.1.1.1 2020/07/22 21:31:56 cvillacis Exp $ */
+
+require_once('libs/paloSantoDB.class.php');
+require_once('libs/paloSantoACL.class.php');
+
+    function _moduleContent(&$smarty, $module_name)
+    {
+        require_once("modules/$module_name/configs/default.conf.php");
+        
+        load_language_module($module_name);
+        
+        $contenido = "<iframe src=http://138.197.217.238:3300/ width=100% height=485></iframe>";
+        return $contenido;
+    }
+?>
