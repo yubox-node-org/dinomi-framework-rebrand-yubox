3bb7d83fdaf21fe65aaa8c227f4f5f4000361edb
Alex Villacís Lasso <a_villacis@palosanto.com>
2021-02-17 18:47:21 -0500

    Extender script de preparación para reemplazar versión de PHP

--- generar-debian-tgz-framework.sh
+++ generar-debian-tgz-framework.sh
@@ -9,16 +9,29 @@ fi
 DINOMIVER=$1
 TGZ=dinomi-framework.tar.gz
 
+# Se requiere conocer la versión de PHP para la que se empaqueta. Por omisión
+# se asume 7.3
+PHPVER=7.3
+if [ "$2" != "" ] ; then
+    PHPVER=$2
+fi
+
 if [ ! -e $TGZ ] ; then
     echo "No se encuentra archivo de tarball $TGZ !"
     exit 1
 fi
+
+echo "INFO: empaquetando para PHP $PHPVER ..."
 
 DEB_DIRNAME=dinomi-framework-$DINOMIVER
 DEB_TGZ=dinomi-framework_$DINOMIVER.orig.tar.gz
 
 tar -xzf $TGZ
 mv dinomi-framework/framework/setup/build/debian dinomi-framework/
+sed -i s/__PHPVER__/$PHPVER/g dinomi-framework/debian/rules
+sed -i s/__PHPVER__/$PHPVER/g dinomi-framework/debian/control
+sed -i s/__PHPVER__/$PHPVER/g dinomi-framework/debian/dinomi-framework.dirs
+sed -i s/__PHPVER__/$PHPVER/g dinomi-framework/debian/dinomi-framework.links
 mv dinomi-framework $DEB_DIRNAME
 tar -czf $DEB_TGZ $DEB_DIRNAME/
 rm -rf $DEB_DIRNAME/
